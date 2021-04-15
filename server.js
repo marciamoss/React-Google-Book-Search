07b2317e70320f9133714bfcd131b58b1bb53fcf
Autumn4eva <45056799+marciamoss@users.noreply.github.com>
2021-04-15 19:18:31 -0400

    fixed typo for local db connection

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhostt/googlebooks",
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
